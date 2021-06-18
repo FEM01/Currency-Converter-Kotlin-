279a28a73237a889895156dd1bd7579f6fdf202d
FEM01 <72187868+FEM01@users.noreply.github.com>
2021-06-19 00:56:10 +0700

    Simple Menu 001

--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -4,22 +4,52 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.log
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tgl : ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        TestButton001.setOnClickListener {
-            Log.i("MainActivity", "Test 001 Berhasil")
-            Toast.makeText(this, "Test 002 Berhasil", Toast.LENGTH_SHORT).show()
+        val drL : DrawerLayout = findViewById(R.id.Home)
+        val nV  : NavigationView = findViewById(R.id.HomeNaviView)
+
+        tgl = ActionBarDrawerToggle(this, drL, R.string.open, R.string.close)
+        drL.addDrawerListener(tgl)
+        tgl.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        nV.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.homeMenuAbout -> Toast.makeText(applicationContext, "About", Toast.LENGTH_SHORT).show()
+                R.id.homeMenuSetting -> Toast.makeText(applicationContext, "Setting", Toast.LENGTH_SHORT).show()
+                R.id.homeMenuFeedback -> Toast.makeText(applicationContext, "Feedback", Toast.LENGTH_SHORT).show()
+
+            }
+            true
         }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (tgl.onOptionsItemSelected(item)){
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
